45e02714a0468db8116261e2ce45dd8af2031b75
hansolu <khs20113@gmail.com>
2023-05-12 11:01:30 +0900

    enum의 비트 연산 샘플

--- shooting2d/Assets/Scripts/CameraClick.cs
+++ shooting2d/Assets/Scripts/CameraClick.cs
@@ -1,12 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+    
 public class CameraClick : MonoBehaviour
-{    
-    // Update is called once per frame
-    void Update()
+{
+    public enum SampleEnum //int
     {
+        None = 0, // 0b 0000 0000
+        AA = 1, // 0b00000001
+        BB = 2, // 0b00000010
+        CC = 4, // 0b00000100
+        DD = 8, // 0b00001000
+        EE = 16
+    }
+    SampleEnum sampleE = SampleEnum.AA | SampleEnum.BB; //이런식으로 체크 가능..
+
+    void CheckEnum()
+    {
+        sampleE.HasFlag(SampleEnum.AA); //AA가 속하는지 여부 체크 가능해짐
+    }
+
+    void Update()
+    {        
         if (Input.GetMouseButtonDown(0))
         {
             //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -18,5 +33,5 @@ public class CameraClick : MonoBehaviour
                 Debug.Log(hit.collider.gameObject.name);
             }
         }       
-    }
+    }   
 }
